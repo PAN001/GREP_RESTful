04f3cfd4f4ba178420b60fb88cd9da08c0fa22bf
PAN001 <pan.atopos@gmail.com>
2018-09-11 03:25:36 -0400

    changes

--- run_debug.py
+++ run_debug.py
@@ -7,6 +7,6 @@ from flask_sslify import SSLify
 # context.use_certificate_file('/root/OtherServer/2_panatopos.top.crt')
 
 context = ('ssl_certificates/Self-signed/panatopos.top.crt', 'ssl_certificates/Self-signed/panatopos.top_ssl_without_pass.key')
-# app.run(debug=True, host='0.0.0.0', port = 443, ssl_context=context)
-app.run(debug=True, host='127.0.0.1', port = 5000, ssl_context=context)
+app.run(debug=True, host='0.0.0.0', port = 443, ssl_context=context)
+# app.run(debug=True, host='127.0.0.1', port = 5000, ssl_context=context)
 sslify = SSLify(app)
